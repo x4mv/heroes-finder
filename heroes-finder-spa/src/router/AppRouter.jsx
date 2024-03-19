54777fa9564826387b49fa9d135d29f1c0e85422
sergio caceres <contactsscu@gmail.com>
2024-03-19 16:20:35 -0300

    Agg clases de active si estoy en la pagina, creando la carpeta ui para los componentes que seran compartidos

--- heroes-finder-spa/src/router/AppRouter.jsx
+++ heroes-finder-spa/src/router/AppRouter.jsx
@@ -2,11 +2,15 @@ import { Route, Routes } from "react-router-dom"
 import { MarvelPage } from "../heroes/pages/MarvelPage"
 import { DcPage } from "../heroes/pages/DcPage"
 import { LoginPage } from "../auth/pages/LoginPage"
+import { Navbar } from "../UI"
 
 
 export const AppRouter = () => {
   return (
     <>
+
+      <Navbar/>
+      
         <Routes>
             <Route path='marvel' element={<MarvelPage/>}/>
             <Route path='dc' element={<DcPage/>}/>
