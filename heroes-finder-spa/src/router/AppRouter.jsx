cf46eebd53940f65b918eb269939bdb5e8599f4a
sergio caceres <contactsscu@gmail.com>
2024-03-19 16:32:36 -0300

    creando archivos barriles para organizar importaciones

--- heroes-finder-spa/src/router/AppRouter.jsx
+++ heroes-finder-spa/src/router/AppRouter.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DcPage"
+import { MarvelPage, DcPage } from '../heroes'
 import { LoginPage } from "../auth/pages/LoginPage"
 import { Navbar } from "../UI"
 
