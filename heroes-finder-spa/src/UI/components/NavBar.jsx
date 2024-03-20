0ab7084bceeb69b441af5164b807d425b2e1996a
sergio caceres <contactsscu@gmail.com>
2024-03-20 16:00:04 -0300

    Creando un navigate para ir del login a la pagina principal, con useNavigate y replace

--- heroes-finder-spa/src/UI/components/NavBar.jsx
+++ heroes-finder-spa/src/UI/components/NavBar.jsx
@@ -1,7 +1,17 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogOut = () => { 
+        navigate('/login',{
+            'replace':true
+        })
+    }
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -57,6 +67,7 @@ export const Navbar = () => {
 
                     <button
                     className='nav-item nav-link btn'
+                    onClick={() => onLogOut()}
                     >
                         Logout
                     </button>
